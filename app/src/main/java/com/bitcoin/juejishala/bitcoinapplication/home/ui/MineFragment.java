ac6173ca228ec00c18a6f136c1fa72511a43ce85
juejishala <602453559@qq.com>
2018-03-22 22:09:52 +0800

    我的页面ui

--- app/src/main/java/com/bitcoin/juejishala/bitcoinapplication/home/ui/MineFragment.java
+++ app/src/main/java/com/bitcoin/juejishala/bitcoinapplication/home/ui/MineFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.bitcoin.juejishala.bitcoinapplication.R;
+import com.bitcoin.juejishala.bitcoinapplication.view.PullZoomView;
 
 /**
  * @author yinyayue
@@ -15,9 +16,41 @@ import com.bitcoin.juejishala.bitcoinapplication.R;
  */
 
 public class MineFragment extends Fragment {
+    private PullZoomView mPullHeader;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mine, null);
+        View view = inflater.inflate(R.layout.fragment_mine, null);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view) {
+        mPullHeader = view.findViewById(R.id.zoom_header);
+        setZoomHeader();
+    }
+
+    private void setZoomHeader() {
+        mPullHeader.setIsParallax(true);
+        mPullHeader.setIsZoomEnable(true);
+        mPullHeader.setSensitive(1.5f);
+        mPullHeader.setZoomTime(500);
+        mPullHeader.setOnScrollListener(new PullZoomView.OnScrollListener() {
+            @Override
+            public void onScroll(int l, int t, int oldl, int oldt) {
+                System.out.println("onScroll   t:" + t + "  oldt:" + oldt);
+            }
+
+            @Override
+            public void onHeaderScroll(int currentY, int maxY) {
+                System.out.println("onHeaderScroll   currentY:" + currentY + "  maxY:" + maxY);
+            }
+
+            @Override
+            public void onContentScroll(int l, int t, int oldl, int oldt) {
+                System.out.println("onContentScroll   t:" + t + "  oldt:" + oldt);
+            }
+        });
     }
 }
